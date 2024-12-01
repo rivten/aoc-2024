2764a30068c051690af44c52ea1b742de8505275
rivten <rivten@riseup.net>
2024-12-01 10:47:12 +0100

    rivten - day 01 - part 2

--- 01/rivten/01.rb
+++ 01/rivten/01.rb
@@ -6,6 +6,14 @@ ARGF.each_line do |line|
   rights << right.to_i
 end
 
+# Part 1
 lefts = lefts.sort
 rights = rights.sort
 p lefts.zip(rights).map{|x| (x[0] - x[1]).abs}.sum
+
+# Part 2
+def similarity_score x, rights
+  rights.filter {|y| y == x}.length
+end
+
+p lefts.map{|x| x * similarity_score(x, rights)}.sum
