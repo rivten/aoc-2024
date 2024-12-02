d59367905ba2b4f80260b7603849f5c402c6fbe4
Sigmanificient <edhyjox@gmail.com>
2024-12-02 13:30:00 +0100

    sigmanificient - day 02 - part 2

--- 02/sigmanificient/main.c
+++ 02/sigmanificient/main.c
@@ -5,6 +5,8 @@
 
 #include "../../01/sigmanificient/helpers.h"
 
+#define ENOUGH_FOR_INPUT 8
+
 size_t parse_report_values(char *line, int *nums)
 {
     size_t count = 0;
@@ -24,25 +26,41 @@ bool is_safe_report(int const *nums, size_t length)
 
     if (length < 2)
         return true;
-
     negative = (nums[1] - nums[0]) < 0;
-    for (size_t i = 0; i < length - 1; i++) {
-        diff = nums[i + 1] - nums[i];
+    for (size_t i = 1; i < length; i++) {
+        diff = nums[i] - nums[i - 1];
 
         if (negative)
             diff = -diff;
-        if (diff < 1 || diff > 3)
+        if (diff <= 0 || diff > 3)
             return false;
     }
     return true;
 }
 
+bool is_safe_report_tolerance(int const *nums, size_t len)
+{
+    int copy[ENOUGH_FOR_INPUT];
+
+    if (is_safe_report(nums + 1, len - 1) || is_safe_report(nums, len - 1))
+        return true;
+
+    for (size_t i = 1; i < len - 1; i++) {
+        memcpy(copy, nums, i * sizeof *nums);
+        memcpy(&copy[i], &nums[i + 1], (len - i) * sizeof *nums);
+        if (is_safe_report(copy, len - 1))
+            return true;
+    }
+    return false;
+}
+
 int main(void)
 {
     char *input;
-    int nums[64];
+    int nums[ENOUGH_FOR_INPUT];
     size_t count;
     size_t safe = 0;
+    size_t safe_with_tolerance = 0;
 
     if (!read_file("input", &input))
         goto stop;
@@ -52,9 +70,11 @@ int main(void)
     ) {
         count = parse_report_values(line, nums);
         safe += is_safe_report(nums, count);
+        safe_with_tolerance += is_safe_report_tolerance(nums, count);
     }
 
     printf("Part 1: %zu\n", safe);
+    printf("Part 2: %zu\n", safe_with_tolerance);
 
 stop:
     return EXIT_SUCCESS;
