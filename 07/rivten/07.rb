f2dc19a2ef2d62e3cd3912e097a347fe35d3ae8f
rivten <rivten@riseup.net>
2024-12-07 09:41:39 +0100

    rivten - day 07 - part 2

--- 07/rivten/07.rb
+++ 07/rivten/07.rb
@@ -6,6 +6,11 @@ equations = ARGF.each_line.map do |line|
   [left, rights]
 end
 
+def concat x, y
+  # i know, i'm cheating :D
+  (x.to_s + y.to_s).to_i
+end
+
 def possible_solve_rec target, elems, solution_count, current_val
   if elems.length == 0 then
     if current_val == target then
@@ -15,9 +20,16 @@ def possible_solve_rec target, elems, solution_count, current_val
     end
   end
 
+  if current_val > target then
+    # main optim
+    # it works because all operations strictly increase the value
+    return solution_count
+  end
+
   x = elems[0]
   solution_count = possible_solve_rec(target, elems[1...], solution_count, current_val + x)
-  possible_solve_rec(target, elems[1...], solution_count, current_val * x)
+  solution_count = possible_solve_rec(target, elems[1...], solution_count, current_val * x)
+  possible_solve_rec(target, elems[1...], solution_count, concat(current_val, x))
 end
 
 def possible_solve eq
