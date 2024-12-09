f3ccc9443fb08f4b3c712b2b06d7403d2a1543bd
Jonas Pedersen <jonas@c.amo.co>
2024-12-09 19:32:08 +0100

    day5 p2

--- 05/whatdo/day5.py
+++ 05/whatdo/day5.py
@@ -11,7 +11,7 @@ class Rule:
     def applies_to(self, n) -> bool:
         return n == self.x or n == self.y
 
-    def is_correct(self, pages: [int]):
+    def is_correct(self, pages: list[int]):
         try:
             first = pages.index(self.x)
             second = pages.index(self.y)
@@ -19,15 +19,27 @@ class Rule:
         except ValueError:
             return True
 
+    def fix(self, pages: list[int]) -> bool:
+        try:
+            first = pages.index(self.x)
+            second = pages.index(self.y)
+            if first > second:
+                pages[first] = self.y
+                pages[second] = self.x
+                return True
+            return False
+        except ValueError:
+            return False
+
 @dataclass
 class Input:
-    rules: [Rule]
-    pages: [[int]]
+    rules: list[Rule]
+    pages: list[list[int]]
 
 def drop(count: int, iterable):
     return list(islice(iterable, 1, None))
 
-def parse(content: [str]) -> Input:
+def parse(content: list[str]) -> Input:
     rules = takewhile(lambda x: x != '\n', content)
     page_list = filter(lambda x: x != '\n', (drop(1, dropwhile(lambda x: x != '\n', content))))
 
@@ -53,15 +65,36 @@ def part1(input: Input) -> int:
     return total
 
 
-def part2(content: Input):
-    pass
+def part2(input: Input):
+    total = 0
+    for pages in input.pages:
+        active_rules = set()
+        for page in pages:
+            for rule in input.rules:
+                if rule.applies_to(page):
+                    #print(f'rule {rule} applies to {pages}')
+                    active_rules.add(rule)
+        
+        if not all([rule.is_correct(pages) for rule in active_rules]):
+            fixed = False
+            while not fixed:
+                fixed = True
+                for rule in active_rules:
+                    if rule.fix(pages):
+                        fixed = False
+                        break
+
+            middle = int(len(pages) / 2) 
+            total += pages[middle]
+    
+    return total
 
 def main():
     with open('day5.txt', 'r') as file:
         content = file.readlines()
     
     input = parse(content)
-    print(part1(input))
+    print(part2(input))
 
 
 if __name__ == '__main__':
