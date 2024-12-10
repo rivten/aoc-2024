c50d60c28e9e4be821328cdc65b93f6b4b593e7e
Yanis Guaye <yguaye44@gmail.com>
2024-12-10 02:20:26 +0100

    misterpemodder - day 09 - part 2

--- 09/misterpemodder/day09.zig
+++ 09/misterpemodder/day09.zig
@@ -11,57 +11,18 @@ pub const std_options = .{
 
 pub const AOC_YEAR: u32 = 2024;
 pub const AOC_DAY: u32 = 9;
-pub const AocData = []const u8;
+pub const AocData = struct {
+    blocks: []u16,
+    max_id: u16,
+};
 pub const AocResult = u64;
 
-pub fn aocPart1(allocator: Allocator, data: AocData) anyerror!AocResult {
+pub fn aocSetup(allocator: Allocator, input: []const u8) anyerror!AocData {
     var blocks = std.ArrayListUnmanaged(u16){};
-    defer blocks.deinit(allocator);
-
-    for (std.mem.trim(u8, data, &std.ascii.whitespace), 0..) |c, i| {
-        const count: u8 = c - '0';
-        if (i % 2 == 0) {
-            try blocks.appendNTimes(allocator, @intCast(i / 2 + 1), @intCast(count));
-        } else {
-            try blocks.appendNTimes(allocator, 0, @intCast(count));
-        }
-    }
-
-    var bs: []u16 = blocks.items;
-    var next_free: ?usize = std.mem.indexOfScalar(u16, bs, 0);
-
-    while (next_free) |free| : (next_free = std.mem.indexOfScalarPos(u16, bs, free, 0)) {
-        bs[free] = bs[bs.len - 1];
-        bs = bs[0 .. bs.len - 1];
-    }
-
-    var checksum: u64 = 0;
-
-    for (bs, 0..) |block, i| {
-        checksum += @as(u64, @intCast(block - 1)) * @as(u64, @intCast(i));
-    }
-
-    return checksum;
-}
-
-fn printLine(bs: []u16) void {
-    for (bs) |block| {
-        if (block > 0) {
-            std.debug.print("{}", .{block - 1});
-        } else {
-            std.debug.print(".", .{});
-        }
-    }
-    std.debug.print("\n", .{});
-}
-
-// 6_227_094_154_536 > X > 5_423_689_415_101
-pub fn aocPart2(allocator: Allocator, data: AocData) anyerror!AocResult {
-    var blocks = std.ArrayListUnmanaged(u16){};
-    defer blocks.deinit(allocator);
+    errdefer blocks.deinit(allocator);
     var max_id: u16 = 0;
 
-    for (std.mem.trim(u8, data, &std.ascii.whitespace), 0..) |c, i| {
+    for (std.mem.trim(u8, input, &std.ascii.whitespace), 0..) |c, i| {
         const count: u8 = c - '0';
         if (i % 2 == 0) {
             try blocks.appendNTimes(allocator, @intCast(i / 2 + 1), @intCast(count));
@@ -70,62 +31,44 @@ pub fn aocPart2(allocator: Allocator, data: AocData) anyerror!AocResult {
             try blocks.appendNTimes(allocator, 0, @intCast(count));
         }
     }
+    return .{
+        .blocks = blocks.items,
+        .max_id = max_id,
+    };
+}
 
-    const bs: []u16 = blocks.items;
-    // var next_free: ?usize = std.mem.indexOfScalar(u16, bs, 0);
+pub fn aocPart1(allocator: Allocator, data: AocData) anyerror!AocResult {
+    var blocks: []u16 = try allocator.alloc(u16, data.blocks.len);
+    @memcpy(blocks, data.blocks); // duplicate blocks to avoid modifying part 2 input
+    var next_free: ?usize = std.mem.indexOfScalar(u16, blocks, 0);
 
-    // printLine(bs);
-    while (max_id > 0) : (max_id -= 1) {
-        const end_pos: usize = std.mem.lastIndexOfScalar(u16, bs, max_id) orelse continue;
-        var start_pos: usize = end_pos;
-
-        while (bs[start_pos] == max_id) : (start_pos -= 1) {
-            if (start_pos == 0) {
-                break;
-            }
-        } else {
-            start_pos += 1;
-        }
-        const len = end_pos - start_pos + 1;
-        // std.debug.print("I: {}, start: {}, end: {}, len: {}\n", .{ max_id - 1, start_pos, end_pos, len });
-        var offset: usize = 0;
-
-        if (bs[start_pos] != max_id or bs[end_pos] != max_id) {
-            std.debug.print("RANGE TOO LARGE: {} at {} to {}\n", .{ max_id, start_pos, end_pos });
-        }
-
-        if ((start_pos > 0 and bs[start_pos - 1] == max_id) or (end_pos + 1 < bs.len and bs[end_pos + 1] == max_id)) {
-            std.debug.print("RANGE TOO SMALL: {} at {} to {}\n", .{ max_id, start_pos, end_pos });
-        }
-
-        while (offset + len < start_pos) {
-            const free_start = std.mem.indexOfScalarPos(u16, bs, offset, 0) orelse break;
-            const free_end = std.mem.indexOfNonePos(u16, bs, free_start, &.{0}) orelse bs.len;
-            const free_len = free_end - free_start;
-
-            if (free_len >= len) {
-                if (std.mem.indexOfNone(u16, bs[free_start .. free_start + len], &.{0})) |z| {
-                    std.debug.print("OVERWRITING DATA: {} at {}\n", .{ bs[z + free_start], z + free_start });
-                }
-                if (std.mem.indexOfNone(u16, bs[start_pos .. end_pos + 1], &.{max_id})) |z| {
-                    std.debug.print("OVERWRITING DATA: {} at {}\n", .{ bs[z + start_pos], z + start_pos });
-                }
-
-                if ((bs[free_start .. free_start + len]).len != (bs[start_pos .. end_pos + 1]).len) {
-                    std.debug.print("LENGTH MISMATCH: {} != {}\n", .{ (bs[free_start .. free_start + len]).len, (bs[start_pos .. end_pos + 1]).len });
-                }
-                @memset(bs[free_start .. free_start + len], max_id);
-                @memset(bs[start_pos .. end_pos + 1], 0);
-                // printLine(bs);
-                break;
-            }
-            offset = free_end;
-        }
+    while (next_free) |free| : (next_free = std.mem.indexOfScalarPos(u16, blocks, free, 0)) {
+        blocks[free] = blocks[blocks.len - 1];
+        blocks = blocks[0 .. blocks.len - 1];
     }
+    return computeChecksum(blocks);
+}
 
+pub fn aocPart2(allocator: Allocator, data: AocData) anyerror!AocResult {
+    _ = allocator;
+    const blocks: []u16 = data.blocks;
+    var file_id: u16 = data.max_id;
+
+    while (file_id > 0) : (file_id -= 1) {
+        const file = findLastFile(blocks, file_id) orelse continue;
+        const file_len = file.end - file.start;
+        const free = findFirstFree(blocks[0..file.start], file_len) orelse continue;
+
+        @memset(blocks[free.start .. free.start + file_len], file_id);
+        @memset(blocks[file.start..file.end], 0);
+    }
+    return computeChecksum(blocks);
+}
+
+fn computeChecksum(blocks: []const u16) u64 {
     var checksum: u64 = 0;
 
-    for (bs, 0..) |block, i| {
+    for (blocks, 0..) |block, i| {
         if (block > 0) {
             checksum += @as(u64, @intCast(block - 1)) * @as(u64, @intCast(i));
         }
@@ -134,6 +77,59 @@ pub fn aocPart2(allocator: Allocator, data: AocData) anyerror!AocResult {
     return checksum;
 }
 
+const BlockSpan = struct {
+    start: usize,
+    end: usize,
+};
+
+fn findLastFile(blocks: []const u16, file_id: u16) ?BlockSpan {
+    const file_end: usize = std.mem.lastIndexOfScalar(u16, blocks, file_id) orelse return null;
+    var file_start: usize = file_end;
+
+    while (blocks[file_start] == file_id) : (file_start -= 1) {
+        if (file_start == 0) {
+            break;
+        }
+    } else {
+        file_start += 1;
+    }
+    return .{
+        .start = file_start,
+        .end = file_end + 1,
+    };
+}
+
+fn findFirstFree(blocks: []const u16, min_len: usize) ?BlockSpan {
+    var free_offset: usize = 0;
+
+    while (free_offset + min_len < blocks.len) {
+        const free_start = std.mem.indexOfScalarPos(u16, blocks, free_offset, 0) orelse break;
+        const free_end = std.mem.indexOfNonePos(u16, blocks, free_start, &.{0}) orelse blocks.len;
+        const free_len = free_end - free_start;
+
+        if (free_len >= min_len) {
+            return .{
+                .start = free_start,
+                .end = free_end,
+            };
+        }
+        free_offset = free_end;
+    }
+    return null;
+}
+
 pub fn main() !void {
     aoc.run(.{});
+}
+
+test "aoc 2024, day 09 example" {
+    const example = "2333133121414131402";
+
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const data: AocData = try aocSetup(allocator, example);
+    try expect(1928 == try aocPart1(allocator, data));
+    try expect(2858 == try aocPart2(allocator, data));
 }
