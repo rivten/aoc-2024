0211c9fb56b1a3816028974234b670a8ab77727f
Cyril Mottier <cyril@amo.co>
2024-12-11 13:26:26 +0100

    Clean Day11 a bit

--- 01/cyrilmottier/aoc-2024/src/main/kotlin/Day11.kt
+++ 01/cyrilmottier/aoc-2024/src/main/kotlin/Day11.kt
@@ -3,23 +3,16 @@ import kotlin.math.log10
 import kotlin.math.pow
 
 fun main() {
-    val cache = mutableMapOf<Int, MutableMap<Long, List<Long>>>()
+    val cache = mutableMapOf<Pair<Long, Int>, List<Long>>()
 
-    fun compute(stone: Long, blinkCount: Int): List<Long> {
-        var blinkCache = cache[blinkCount]
-        if (blinkCache == null) {
-            blinkCache = mutableMapOf<Long, List<Long>>().also {
-                cache[blinkCount] = it
-            }
-        }
-        val stoneCountCache = blinkCache[stone]
-        if (stoneCountCache != null) {
-            return stoneCountCache
+    fun blink(stone: Long, blinkCount: Int): List<Long> {
+        val blinkCache = cache[stone to blinkCount]
+        if (blinkCache != null) {
+            return blinkCache
         }
 
         val digits = floor(log10(stone.toDouble()) + 1).toInt()
 
-        // General case
         val result = when {
             stone == 0L -> listOf(1L)
             digits % 2 == 0 -> {
@@ -30,7 +23,7 @@ fun main() {
             else -> listOf(stone * 2024)
         }
 
-        blinkCache[stone] = result
+        cache[stone to blinkCount] = result
 
         return result
     }
@@ -39,7 +32,7 @@ fun main() {
         if (blinkCount == 0) {
             return stones
         }
-        val newStones = stones.flatMap { compute(it, blinkCount) }
+        val newStones = stones.flatMap { blink(it, blinkCount) }
         return compute(newStones, blinkCount - 1)
     }
 
--- 01/cyrilmottier/aoc-2024/src/main/kotlin/Day11_2.kt
+++ 01/cyrilmottier/aoc-2024/src/main/kotlin/Day11_2.kt
@@ -3,53 +3,37 @@ import kotlin.math.log10
 import kotlin.math.pow
 
 fun main() {
-    val cache = mutableMapOf<Int, MutableMap<Long, Long>>()
-    val listCache = mutableMapOf<Long, List<Long>>()
+    val cache = mutableMapOf<Pair<Long, Int>, Long>()
 
-    fun apply(stone: Long): List<Long> {
-        val digits = floor(log10(stone.toDouble()) + 1).toInt()
-        return when {
-            stone == 0L -> listOf(1L)
-            digits % 2 == 0 -> {
-                val divider = 10.0.pow(digits / 2).toLong()
-                listOf(stone / divider, stone % divider)
+    fun blink(stone: Long): List<Long> {
+        return buildList {
+            val digits = floor(log10(stone.toDouble()) + 1).toInt()
+            when {
+                stone == 0L -> add(1L)
+                digits % 2 == 0 -> {
+                    val divider = 10.0.pow(digits / 2).toLong()
+                    add(stone / divider)
+                    add(stone % divider)
+                }
+
+                else -> add(stone * 2024)
             }
-
-            else -> listOf(stone * 2024)
         }
     }
 
-    fun rule(stone: Long): List<Long> {
-        val c = listCache[stone]
-        if (c != null) return c
-        return apply(stone).also {
-            listCache[stone] = it
-        }
-    }
-
-    fun compute(stone: Long, blinkCount: Int): Long {
-        var blinkCache = cache[blinkCount]
-        if (blinkCache == null) {
-            blinkCache = mutableMapOf<Long, Long>().also {
-                cache[blinkCount] = it
+    fun blink(stone: Long, blinkCount: Int): Long = if (blinkCount == 0) {
+        1L
+    } else {
+        cache[stone to blinkCount] ?: blink(stone)
+            .sumOf { blink(it, blinkCount - 1) }.also {
+                cache[stone to blinkCount] = it
             }
-        }
-        val stoneCountCache = blinkCache[stone]
-        if (stoneCountCache != null) {
-            return stoneCountCache
-        }
-
-        if (blinkCount == 0) return 1L
-
-        return rule(stone).sumOf { compute(it, blinkCount - 1) }.also {
-            blinkCache[stone] = it
-        }
     }
 
     val stones = readInput("Day11")[0]
         .split(" ")
         .map { it.toLong() }
-        .sumOf { compute(it, 75) }
+        .sumOf { blink(it, 75) }
 
     println(stones)
 }
