2cbd6ed9d6c1e32d3f54edccb10410fb73bf5751
rivten <rivten@riseup.net>
2024-12-12 13:47:20 +0100

    rivten - day 12 - part 2

--- 12/rivten/12.rb
+++ 12/rivten/12.rb
@@ -54,4 +54,55 @@ def price region
   region.length * perimeter(region)
 end
 
+def fences region
+  fences = []
+  region.each do |p|
+    DIRS.filter{|d| !region.include?(p + d)}.each do |d|
+      fences << [p, d]
+    end
+  end
+  fences
+end
+
+def build_fence_graph fences
+  g = Hash.new {|h, k| h[k] = []}
+  fences.each do |f|
+    p, d = f
+    if d.real == 0 then
+      # horizontal bar
+      g[f] = fences.filter{|op, od| od.real == 0 && (od - d).imaginary == 0 && (op - p).real.abs == 1 && (op - p).imaginary == 0}
+    elsif d.imaginary == 0 then
+      # vertical bar
+      g[f] = fences.filter{|op, od| od.imaginary == 0 && (od - d).real == 0 && (op - p).imaginary.abs == 1 && (op - p).real == 0}
+    else
+      raise "impossible"
+    end
+  end
+  g
+end
+
+def graph_connex_count g
+  visited = Set.new
+  count = 0
+  for start_fence, adj_fences in g do
+    next if visited.include? start_fence
+    count += 1
+    stack = [start_fence]
+    while !stack.empty? do
+      f = stack.pop
+      next if visited.include? f
+      visited.add f
+      for adj_f in g[f] do
+        stack << adj_f
+      end
+    end
+  end
+  count
+end
+
+def bulk_price region
+  region.length * graph_connex_count(build_fence_graph(fences region))
+end
+
 p regions.sum{|r| price r}
+p regions.sum{|r| bulk_price r}
