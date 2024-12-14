308eaf839e3a220097606d85700b69cc8a13264f
rivten <rivten@riseup.net>
2024-12-14 11:38:12 +0100

    rivten - day 14 - part 2
    best part 2 ever

--- 14/14.rb
+++ 14/14.rb
@@ -13,15 +13,64 @@ def advance r
   r[:p] = (r[:p].real % WIDTH) + (r[:p].imaginary % HEIGHT) * 1i
 end
 
-100.times do 
-  robots.each do |r|
-    advance r
+def draw robots
+  for y in 0...HEIGHT do
+    line = ""
+    for x in 0...WIDTH do
+      count = robots.count{|r| r[:p] == x + y * 1i}
+      if count == 0 then
+        line += "."
+      else
+        line += count.to_s
+      end
+    end
+    puts line
   end
 end
 
-q1 = robots.count{|r| r[:p].real < (WIDTH / 2) && r[:p].imaginary < (HEIGHT / 2)}
-q2 = robots.count{|r| r[:p].real < (WIDTH / 2) && r[:p].imaginary > (HEIGHT / 2)}
-q3 = robots.count{|r| r[:p].real > (WIDTH / 2) && r[:p].imaginary < (HEIGHT / 2)}
-q4 = robots.count{|r| r[:p].real > (WIDTH / 2) && r[:p].imaginary > (HEIGHT / 2)}
+analysis = false
+if analysis then
+  require 'csv'
+  CSV.open("out.csv", "wb") do |csv|
+    100000.times do |sec|
+      robots.each do |r|
+        advance r
+      end
+      q1 = robots.count{|r| r[:p].real < (WIDTH / 2) && r[:p].imaginary < (HEIGHT / 2)}
+      q2 = robots.count{|r| r[:p].real < (WIDTH / 2) && r[:p].imaginary > (HEIGHT / 2)}
+      q3 = robots.count{|r| r[:p].real > (WIDTH / 2) && r[:p].imaginary < (HEIGHT / 2)}
+      q4 = robots.count{|r| r[:p].real > (WIDTH / 2) && r[:p].imaginary > (HEIGHT / 2)}
+      q = q1 * q2 * q3 * q4
 
-p q1 * q2 * q3 * q4
+      csv << [sec, q1 * q2 * q3 * q4]
+
+      ## gnuplot script used to see the csv and determine the rare data
+      #
+      # set datafile separator ","
+      # plot 'out.csv' using 1:2
+      #
+    end
+  end
+else
+  100000.times do |sec|
+    robots.each do |r|
+      advance r
+    end
+    q1 = robots.count{|r| r[:p].real < (WIDTH / 2) && r[:p].imaginary < (HEIGHT / 2)}
+    q2 = robots.count{|r| r[:p].real < (WIDTH / 2) && r[:p].imaginary > (HEIGHT / 2)}
+    q3 = robots.count{|r| r[:p].real > (WIDTH / 2) && r[:p].imaginary < (HEIGHT / 2)}
+    q4 = robots.count{|r| r[:p].real > (WIDTH / 2) && r[:p].imaginary > (HEIGHT / 2)}
+    q = q1 * q2 * q3 * q4
+
+
+    # determined by analysis
+    threshold = 50000000
+    if q <  threshold then
+      p sec + 1
+      draw robots
+      break
+    end
+  end
+end
+
+
